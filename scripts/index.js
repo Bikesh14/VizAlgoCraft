64e9f95a1506883b9d931c55cb5d13ddd3f749bf
Bikesh Bimali <bimalibikesh@gmail.com>
2023-12-13 10:52:29 +0545

    add: speed for algorithm visualizer

--- scripts/index.js
+++ scripts/index.js
@@ -5,20 +5,6 @@ const ctx = canvas.getContext("2d");
 let notCalledYet = true;
 const numberOfCompares = document.getElementById("compare-count");
 
-// function updateArray() {
-//   const customArray = document.getElementById("custom-array").value.trim();
-//   try {
-//     elements_array = JSON.parse(customArray);
-//     console.log(elements_array);
-//     console.log(typeof elements_array);
-//     initLines();
-//     animate();
-//     console.log("Updated Array:", elements_array);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
-
 let lines = [];
 let elements_array = [100, 80, 10, 200, 300, 50, 600];
 // const elements_array = randomArrayGenerator(configurations.number_of_elements);
@@ -40,9 +26,7 @@ const initLines = () => {
 };
 
 const customArraySection = document.getElementById("custom-array-section");
-document
-  .getElementById("array-options")
-  .addEventListener("change", function () {
+document.getElementById("array-options").addEventListener("change", function () {
     const selectedOption = this.value;
     if (selectedOption === "custom") {
       customArraySection.style.display = "block";
