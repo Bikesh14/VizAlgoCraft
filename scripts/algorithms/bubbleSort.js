9c8684cdb164c9f59f44014205f793e4086c8284
Bikesh Bimali <bimalibikesh@gmail.com>
2023-12-15 14:14:23 +0545

    fix: using performance.now() instead of Date.now() to read in milliseconds

--- scripts/algorithms/bubbleSort.js
+++ scripts/algorithms/bubbleSort.js
@@ -4,7 +4,8 @@
  * @returns {Object[]} - An array of sorting actions representing the steps of the algorithm.
  */
 const bubbleSort = (array) => {
-  let startTime = Date.now();
+  //Combining millisecond resolution of Date.now() with the microsecond resolution of performance.now()
+  let startMicroTime = performance.now();
   let actions = [];
   for (let i = 0; i < array.length; i++) {
     for (let j = 0; j < array.length - i - 1; j++) {
@@ -18,15 +19,10 @@ const bubbleSort = (array) => {
     }
     actions.push(sortActions.sorted(array.length - i - 1));
   }
-  let endTime = Date.now();
-
-  let timeRequired = endTime - startTime;
-  console.log(startTime);
-  console.log(endTime);
-  console.log(
-    "Time required------------------",
-    timeRequired.toFixed(6),
-    "milliseconds"
-  );
+  let endMicroTime = performance.now();
+  let timeRequired = (endMicroTime - startMicroTime).toFixed(6);
+  console.log(startMicroTime);
+  console.log(endMicroTime);
+  console.log("Time required------------------", timeRequired, "milliseconds");
   return actions;
 };
