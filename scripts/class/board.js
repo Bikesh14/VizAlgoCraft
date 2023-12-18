24513e8fd6f1debb880fda5fb0e516bff53aec0b
Bikesh Bimali <bimalibikesh@gmail.com>
2023-12-18 08:55:24 +0545

    fix: adding the same deleted file to update it in Github

---
+++ scripts/class/board.js
@@ -0,0 +1,135 @@
+class Board {
+  constructor() {
+    this.rows = parseInt(height / scale);
+    this.cols = parseInt(width / scale);
+    this.grid = [];
+    this.heuristics = false;
+
+    for (let i = 0; i < this.rows; i++) {
+      let row = [];
+      for (let j = 0; j < this.cols; j++) {
+        row.push(new Cell(j, i)); //populate with Cell object
+      }
+      this.grid.push(row);
+    }
+
+    // start and goal positions
+    let Y1 = 1;
+    let Y2 = parseInt(this.rows - 2);
+    let X1 = 1;
+    let X2 = parseInt(this.cols - 2);
+
+    this.grid[Y1][X1].start = true;
+    this.grid[Y2][X2].goal = true;
+
+    this.start = this.grid[Y1][X1];
+    this.goal = this.grid[Y2][X2];
+  }
+
+  // Display all cells
+  show() {
+    context.clearRect(0, 0, width, height);
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
+        this.grid[i][j].show();
+      }
+    }
+  }
+
+  async reset() {
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
+        this.grid[i][j].isWall = false;
+        this.grid[i][j].hasBeenDiscovered = false;
+        this.grid[i][j].parent = false;
+        this.grid[i][j].weight = 0;
+      }
+    }
+    this.show();
+  }
+
+  async resetWeights() {
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
+        this.grid[i][j].hasBeenDiscovered = false;
+        this.grid[i][j].parent = false;
+        this.grid[i][j].weight = 0;
+      }
+    }
+    this.show();
+  }
+
+  clearPath() {
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
+        this.grid[i][j].hasBeenDiscovered = false;
+        this.grid[i][j].parent = false;
+      }
+    }
+    this.show();
+  }
+
+  // Add a cell at a particular index
+  addWall(x, y) {
+    if (y >= this.rows || x >= this.cols || y < 0 || x < 0) {
+      return;
+    }
+    if (this.grid[y][x].start == true || this.grid[y][x].goal == true) {
+      return;
+    }
+    this.grid[y][x].isWall = true;
+    this.grid[y][x].weight = Infinity;
+    this.grid[y][x].show();
+  }
+
+  // Toggle isWall at particular index
+  toggleWall(x, y) {
+    if (y >= this.rows || x >= this.cols || y < 0 || x < 0) {
+      return;
+    }
+    this.grid[y][x].toggleWall();
+    this.grid[y][x].show();
+  }
+  // Returns the neighbors of a cell
+  getNeighbors(cell) {
+    let i = cell.y;
+    let j = cell.x;
+    let neighbors = [];
+
+    if (i != this.rows - 1) {
+      neighbors.push([i + 1, j]);
+    }
+    if (i > 0) {
+      neighbors.push([i - 1, j]);
+    }
+    if (j != this.cols - 1) {
+      neighbors.push([i, j + 1]);
+    }
+    if (j > 0) {
+      neighbors.push([i, j - 1]);
+    }
+
+    return neighbors;
+  }
+
+  async initializeHeuristics() {
+    // if heuristics are already initialized, reset the gScore
+    if (this.heuristics) {
+      for (let i = 0; i < this.rows; i++) {
+        for (let j = 0; j < this.cols; j++) {
+          this.grid[i][j].gScore = 0;
+        }
+      }
+    } else {
+      for (let i = 0; i < this.rows; i++) {
+        for (let j = 0; j < this.cols; j++) {
+          this.grid[i][j].gScore = 0;
+          this.grid[i][j].hScore =
+            Math.abs(this.grid[i][j].x - this.goal.x) +
+            Math.abs(this.grid[i][j].y - this.goal.y);
+        }
+      }
+      this.heuristics = true;
+    }
+  }
+}
---
+++ scripts/class/cell.js
@@ -0,0 +1,121 @@
+class Cell {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.isWall = false;
+    this.hasBeenDiscovered = false;
+    this.parent = false;
+    this.weight = 0;
+    this.start = false;
+    this.goal = false;
+  }
+
+  // Toggle the Wall
+  toggleWall() {
+    if (this.start || this.goal) {
+      return;
+    }
+    if (this.isWall) {
+      this.isWall = false;
+      this.weight = 0;
+    } else {
+      this.isWall = true;
+      this.weight = Infinity;
+    }
+  }
+
+  async visitNode() {
+    sleep(2500 / fps).then(() => {
+      this.show(visitedColor);
+    });
+  }
+
+  async discoverNode(parent) {
+    this.hasBeenDiscovered = true;
+    this.parent = parent;
+    this.show(custom_alert_color);
+    sleep(2500 / fps).then(() => {
+      this.show(discoveredColor);
+    });
+    await sleep(1000 / fps);
+  }
+
+  async show(color) {
+    // Default fill and stroke
+    context.lineWidth = 1;
+    context.strokeStyle = defaultBorder;
+    context.fillStyle = defaultColor;
+
+    if (this.start) {
+      let image = new Image();
+      image.src = startImg;
+      image.onload = () => {
+        context.clearRect(this.x * scale, this.y * scale, scale, scale);
+        context.drawImage(
+          image,
+          this.x * scale + 2,
+          this.y * scale + 2,
+          scale - 4,
+          scale - 4
+        );
+      };
+      return;
+    } else if (this.goal) {
+      let image = new Image();
+      image.src = endImg;
+      image.onload = () => {
+        context.clearRect(this.x * scale, this.y * scale, scale, scale);
+        context.drawImage(
+          image,
+          this.x * scale + 2,
+          this.y * scale + 2,
+          scale - 2,
+          scale - 2
+        );
+      };
+      return;
+    } else if (this.isWall) {
+      context.fillStyle = custom_black;
+    } else if (color) {
+      context.fillStyle = color;
+    } else if (this.hasBeenDiscovered) {
+      context.fillStyle = seenColor;
+    }
+
+    context.strokeRect(
+      this.x * scale + offset,
+      this.y * scale + offset,
+      scale - 2 * offset,
+      scale - 2 * offset
+    );
+    context.fillRect(
+      this.x * scale + offset,
+      this.y * scale + offset,
+      scale - 2 * offset,
+      scale - 2 * offset
+    );
+  }
+
+  async drawPathLine(prev) {
+    //line width of final path mapper
+    context.lineWidth = 4;
+    context.strokeStyle = custom_green;
+    let beginX = this.x * scale + scale / 2;
+    let beginY = this.y * scale + scale / 2;
+    let endX = prev.x * scale + scale / 2;
+    let endY = prev.y * scale + scale / 2;
+    context.beginPath();
+    context.moveTo(beginX, beginY);
+    context.lineTo(endX, endY);
+    context.stroke(); //draw line
+    context.closePath();
+
+    // adding these so that the start and goal images dont get overlapped
+    if (prev == board.start) {
+      board.start.show();
+    }
+    if (this == board.goal) {
+      board.goal.show();
+    }
+  }
+}
